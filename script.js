34e00eba5b9328ee0d177ef11a863746dda19fe7
luciabertino <luciabert123@gmail.com>
2026-06-13 17:33:11 -0400

    agrege

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+// Animación simple al cargar
+window.addEventListener("load", () => {
+  document.body.style.opacity = "1";
+});
+
 // Scroll suave
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
   anchor.addEventListener("click", function(e) {
